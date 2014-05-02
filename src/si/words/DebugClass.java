92ddc0ae7ced13202ee11561013f965fc59c5e60
SirKenzi <st.kenzi@gmail.com>
2014-05-02 22:26:11 +0200

    Test Commit
    danke schon

---
+++ src/si/words/DebugClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package si.words;
+
+/**
+ *
+ * @author Kenzi
+ */
+public class DebugClass {
+    
+    private int var;
+    
+    DebugClass(int var){
+        this.var = var;
+    }
+    
+    
+}
---
+++ src/si/words/SiWords.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package si.words;
+
+/**
+ *
+ * @author Kenzi
+ */
+public class SiWords {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Github test");
+        DebugClass testObj = new DebugClass(1);
+    }
+    
+}
